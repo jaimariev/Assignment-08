e9518dcc4551c568c5e30c2b6a2cdc38f7ea4e52
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-10-19 16:57:00 -0700

    added notes

--- main.js
+++ main.js
@@ -47,10 +47,33 @@ var costBetween = items.filter(function(priceObj) {
 // *Medium Size, Welcome To Our Firepit- Where Friends and Marshmallows get Toasted	At The Same Time-Painted Wood Sign-customed Color is made of wood.
 // *Magnetic Wll Mount Bottle Opener Barware Set- Stainless steel or black - Personalized if you like! is made of wood.
 // *Engraved Pocket knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 knives is made of wood.
+	var itemsMadeOfWood = madeOfWood();
+
+	itemsMadeOfWood.forEach(function(item){
+		$('#s4').append("<p>" + item + " is made of wood. </p>")
+	});
+});//this puts it into your html
 
 
+function madeOfWood() {
+	var newArray = items.filter(function(item){
+		var materials = item.materials;
+		var isWood = false;
 
+		materials.forEach(function(material){
+			if (material === 'wood') {
+				isWood = true;
+			}
+		});
 
+		return isWood;
+	}).map(function(item){
+		return item.title;
+	});
+	return newArray;
+}
+
+//NOTES: var newArray =... grabs from the item arrays. var materials to item.materials is grabbing the all the items under the materials section. var iswood claiming false at first will help the loop. materials.foreach is another function grabbing the materials from the list. if(materials === 'wood') iswood = true then will grab anything from the materials array that is made of wood. return iswood would then return anything to you that is made of wood. map then grabs the title and boom.
 
 
 
@@ -60,14 +83,13 @@ var costBetween = items.filter(function(priceObj) {
 // *The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug has 13 materials.
 // *glass, sandblast cabinet, vinyl, beer glass, pint glass, etched pint glass, etched glass, etched beer glass, 16 oz pint, beer gift, etched harry potter glass, the three broomsticks glass, personalized harry potter glass.
 
-
-
+//do the filter array with a length. then map title and materials. append twice into html **remember**
 
 
 
 //How many items were made by their sellers?
 // *18 were made by their sellers
 
-
+//filter
 
 
